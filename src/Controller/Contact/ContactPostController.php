04db7a4787d9112270f45a6e4eaa4a3e540ac692
Billy VILLENA <coding@devrendal.fr>
2024-04-08 13:20:03 +0200

    ✨ [Contact] Mail send
    * 🐋 [docker-compose] Add service "mailcatcher"

--- src/Controller/Contact/ContactPostController.php
+++ src/Controller/Contact/ContactPostController.php
@@ -10,6 +10,9 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
@@ -19,6 +22,12 @@ use Symfony\Component\Routing\Attribute\Route;
 )]
 final class ContactPostController extends AbstractController
 {
+    public function __construct(
+        private MailerInterface $mailer
+    )
+    {
+    }
+
     public function __invoke(
         LoggerInterface $logger,
         Request $request
@@ -40,6 +49,7 @@ final class ContactPostController extends AbstractController
                     'message' => $contactModel->getMessage(),
                 ]
             );
+            $this->sendMail($contactModel);
 
             return new Response('Formulaire envoyé !');
         }
@@ -50,5 +60,37 @@ final class ContactPostController extends AbstractController
                 'contact_form' => $contactForm,
             ]
         );
+    }
+
+    private function sendMail(ContactModel $contactModel): void
+    {
+        $email = new Email();
+        $email->sender( // Qui envoie le mail ?
+            new Address(
+                $contactModel->getEmail(),
+                $contactModel->getName()
+            )
+        );
+        $email->to( // Qui reçoit le mail ?
+            new Address(
+                'billy.villena@lyon.ort.asso.fr',
+                'Billy Framework 3OLEN'
+            )
+        );
+        $email->subject('Message de contact');
+        $email->text(
+            "Bonjour,\n"
+            . "Je vous contacte selon :\n"
+            . $contactModel->getMessage()
+            . "\nDate d'envoi de message : "
+            . (new \DateTime())->format('d/m/Y')
+            . "\nDate de rdv voulue : "
+            . $contactModel->getAppointmentDate()->format('d/m/Y')
+            . "\n\nMerci,\n"
+            . "Bonne journée.\n"
+            . $contactModel->getName()
+        );
+
+        $this->mailer->send($email);
     }
 }
